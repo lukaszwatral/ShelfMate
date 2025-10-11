02950131b0f24c01c3555f2283fc8b5f16df3e72
Łukasz Watral <lukaszwatral@gmail.com>
2025-10-11 13:35:32 +0200

    initial vue

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+
+<!--  <RouterView />-->
+</template>
+
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+<style>
+</style>
